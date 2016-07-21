f99a2a55ac4504d71a45d7abce2395c5d4fc63e4
Chris <krzysztofsek@outlook.com>
2016-07-21 15:08:37 +0200

    Added serialization

--- ExpandObject/Program.cs
+++ ExpandObject/Program.cs
@@ -4,6 +4,7 @@ using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 
 namespace ExpandObject
@@ -24,7 +25,24 @@ namespace ExpandObject
 
             AddProperty(expando, "Language", "Polish"); // Language added
 
+            AddProperty(expando, "MyBirthData", new BirthInfo() {Date = DateTime.Now, Place = "Kraków"});
+
             Console.WriteLine($"My name is {expando.Name} and my language is {expando.Language}");
+
+            // Attempt 2
+
+
+            var myAnonymousObj = new
+            {
+                expando
+            };
+
+            var serialized = JsonConvert.SerializeObject(myAnonymousObj, Formatting.Indented);
+
+
+
+
+
         }
 
         static void AddProperty(ExpandoObject expando, string propertyName, object propertyValue)
@@ -36,4 +54,26 @@ namespace ExpandObject
                 expandoDict.Add(propertyName, propertyValue);
         }
     }
+
+
+    public class BirthInfo
+    {
+        public string Place { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public static class DynamicBuilder
+    {
+        static void AddProperty(ExpandoObject expando, string propertyName, object propertyValue)
+        {
+            var expandoDict = expando as IDictionary<string, object>;
+            if (expandoDict.ContainsKey(propertyName))
+                expandoDict[propertyName] = propertyValue;
+            else
+                expandoDict.Add(propertyName, propertyValue);
+        }
+
+
+    }
+
 }
